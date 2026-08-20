37dbc6ed26091a36357cd8b106452578066ce2ca
txt507177 <17817150489@163.com>
2026-08-20 17:01:24 +0800

    2

--- frm1.cs
+++ frm1.cs
@@ -20,7 +20,7 @@ namespace dobihara2
         public void frm1_Load(object sender, EventArgs e)
         {
             csy1 = this.ClientSize.Height/2 - 200;
-            csx1 = this.ClientSize.Width / 2;//this.Controls.Count;//
+            csx1 = 0;//this.Controls.Count;//
         }
         public void load1(object sender, EventArgs e)
         {
@@ -29,8 +29,8 @@ namespace dobihara2
         public void load2(object sender, EventArgs e)
         {
             pb1.Image = di1;
-            pb1.Width = 1200;
-            pb1.Height = 500;
+            pb1.Width = 1980;
+            pb1.Height = 400;
             //pb1.SizeMode = PictureBoxSizeMode.Zoom;
             pb1.Location = new Point(csx1, csy1);
             //pb1.Location = new Point(this.Controls.Count, 400);
@@ -39,13 +39,13 @@ namespace dobihara2
         public void resw1(object sender, EventArgs e)
         {
             csy1 = this.ClientSize.Height/2 - 200;
-            csx1 = this.ClientSize.Width/2;
+            csx1 = 0;
         }
         public void resw2(object sender, EventArgs e)
         {
             pb1.Location = new Point(csx1, csy1);
-            //MessageBox.Show(this.ClientSize.Width.ToString());
-            pb1.Width = this.ClientSize.Width;
+            MessageBox.Show(this.ClientSize.Height.ToString());
+            
             //pb1.Height = 500;
         }
     }
